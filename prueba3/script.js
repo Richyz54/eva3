cc9c798a6e5139734323db8f98ea53bbf06f8d27
Richyz54 <103473274+Richyz54@users.noreply.github.com>
2022-07-12 21:49:21 -0400

    eva3
    eva3

---
+++ prueba3/script.js
@@ -0,0 +1,32 @@
+function validar()
+    {
+        var nombre= document.formulario.txt_nombre.value;
+        var rut= document.formulario.txa_rut.value;
+        var cell= document.formulario.txt_cell.value;
+        guion1 = nombre.charAt(9)
+        if(nombre <5 || nombre >30)
+        {
+            alert("nombrer debe tener minimo 3 y maximo 30")
+            document.formulario.txa_rut.focus()
+            return false
+        }
+        if(rut <9 )
+        {
+            alert("rut incompleto, deve ingresarlo con guion y sin puntos")
+            document.formulario.txa_rut.focus()
+            return false
+        }
+        if(guion1 != ("-"))
+        {
+            alert("rut incorrecto favor verificar guion")
+            document.formulario.txa_rut.focus()
+            return false
+        }
+        if(cell <9)
+        {
+            alert("Telefonoi debe tener 9 digitos")
+            document.formulario.txa_rut.focus()
+            return false
+        }
+
+    }
